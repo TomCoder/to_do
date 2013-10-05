740bbe3fa2980173fec28614d4016d95a6c485ba
Thomas Mugo <tom1mt@yahoo.com>
2013-10-05 13:18:08 +0300

    List urls

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from main import views
 urlpatterns = patterns('',
         url(r'^$', views.home, name='home'),
+        url(r'^list/$', views.to_dos, name='to_dos'),
         #url(r'^news/(?P<news_id>\d+)/$', views.news_view, name='news_view'),
         #url(r'^about/$', views.about, name='about'),
         #url(r'^contact/$', views.contact, name='contact'),
