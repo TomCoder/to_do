13cc4490ab638f701c7baa25ba6768224d816493
Nickson Kaigi <nickaigi@gmail.com>
2013-10-05 13:59:41 +0300

    almost done

--- main/views.py
+++ main/views.py
@@ -1,12 +1,28 @@
 # Create your views here.
+import datetime
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 
 from main.models import Event
 
+
 def home(request):
     if request.method == "POST":
-        import pdb; pdb.set_trace() 
+        #import pdb; pdb.set_trace() 
+        title = request.POST["title"]
+        description = request.POST["description"]
+        venue = request.POST["venue"]
+        date = request.POST["date"].split("-")
+        event = Event(
+                title=title,
+                description=description,
+                venue=venue,
+                date=datetime.date(int(date[0]), int(date[1]), int(date[2]))
+                )
+        event.save()
+        return HttpResponseRedirect("/list")
+        #import pdb; pdb.set_trace()
 
     return render_to_response("create.html", {}, context_instance=RequestContext(request))
 
--- to_do/settings.py
+++ to_do/settings.py
@@ -35,7 +35,7 @@ else:
             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'todo',                      # Or path to database file if using sqlite3.
             'USER': 'root',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
+            'PASSWORD': '5a5aafr1ca',                  # Not used with sqlite3.
             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
